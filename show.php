d25a2ef58b72d1c9bc6df3e0dcd42ce64d2f513c
b <vpbalint@silentsignal.hu>
2019-10-08 16:45:17 +0200

    typofix

--- show.php
+++ show.php
@@ -3,6 +3,6 @@ $target_dir="/app/instance/uploads/"
 
 header("Content-Type: image/png");
 
-echo file_get_contents($targer_dir.$_GET["id"].".png");
+echo file_get_contents($target_dir.$_GET["id"].".png");
 
 ?>
